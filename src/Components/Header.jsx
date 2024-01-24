4fe5614811adb3b990935deca3f23a0cdfdf2785
Yaggi <yaggim@gmail.com>
2024-01-24 01:19:16 -0300

    Se agrego dropdown con Dark y light theme

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -2,6 +2,8 @@ import React, { useState } from 'react'
 import logo from '../assets/logo-mobile.svg';
 import iconDown from '../assets/icon-chevron-down.svg';
 import iconUp from '../assets/icon-chevron-up.svg'
+import elipsis from '../assets/icon-vertical-ellipsis.svg';
+import HeaderDropdown from './HeaderDropdown';
 
 function Header() {
 
@@ -21,7 +23,7 @@ function Header() {
                     kanban
                 </h3>
 
-                <div>
+                <div className=' flex items-center'>
                     <h3 className=" truncate max-w-[200px] md:text-2xl text-xl font-bold md:ml-20 font-sans  ">
                         board Name
                     </h3>
@@ -31,8 +33,23 @@ function Header() {
                 </div>
             </div>
 
+            {/*Right side*/}
+
+            <div className=" flex space-x-4 items-center md:space-x-6">
+                <button className="hidden md:block button">
+                    + Add new task
+                </button>
+
+                <button className="button py-1 px-3 md:hidden">
+                    +
+                </button>
+
+                <img src={elipsis} alt="elipsis" className=" cursor-pointer h-6 " />
+            </div>
 
         </header>
+
+        {openDropdown && <HeaderDropdown setOpenDropdown={setOpenDropdown}/>}
     </div>
   )
 }
